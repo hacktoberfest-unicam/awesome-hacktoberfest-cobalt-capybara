414ede818f3c1bca330f851b329a8eefd46ed964
Frascott05 <francesco2005scotti@gmail.com>
2024-10-16 12:30:40 +0200

    palestra di Edgar

---
+++ Palestra di Edsger Dijkstra (ASD)/Pokemon Battle Damage Calculator/team24/battle_calculation.py
@@ -0,0 +1,7 @@
+import Pokemon
+
+bulbasaur = Pokemon("Bulbasaur", "Grass", 49, 49, ["Vine Whip", "Razor Leaf"], ["Fire"])
+charmander = Pokemon("Charmander", "Fire", 52, 43, ["Ember", "Flamethrower"], ["Grass"])
+
+damage = charmander.calculate_damage(bulbasaur)
+print(f"Charmander infligge {damage} danni a Bulbasaur")
---
+++ Palestra di Edsger Dijkstra (ASD)/Research/team24/search.py
@@ -0,0 +1,23 @@
+def linear_search(pokedex, name_or_id):
+    for pokemon in pokedex:
+        if pokemon["nome"] == name_or_id or pokemon["id"] == name_or_id:
+            return pokemon
+    return None
+
+def main():
+    pokedex = [
+        {"id": 1, "nome": "Bulbasaur"},
+        {"id": 2, "nome": "Ivysaur"},
+        {"id": 3, "nome": "Venusaur"},
+        {"id": 4, "nome": "Charmander"},
+    ]
+    
+    search_key = "Charmander"
+    result = linear_search(pokedex, search_key)
+    
+    if result is not None:
+        print("Pokémon trovato: " + str(result)) 
+    else:
+        print("Pokémon non trovato.")
+
+main()
