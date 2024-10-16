8214ed6bbd98354f1d14dc21d70aa3765a6304fa
Francesco Valentini <francesco.valentini930@gmail.com>
2024-10-16 16:10:03 +0200

    Create Pokemon.java

---
+++ Palestra di Dennis Ritchie (IMP)/Pokemon Battle/22-PokemonBattle/Pokemon.java
@@ -0,0 +1,31 @@
+
+class Pokemon {
+    String name;
+    int hp;
+    int attack;
+    int defense;
+
+    public Pokemon(String name, int hp, int attack, int defense) {
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    public int attack(Pokemon opponent) {
+        int damage = this.attack - opponent.defense;
+        
+        damage = Math.max(damage, 0);
+        opponent.hp -= damage;
+        System.out.println(this.name + " attacca " + opponent.name + " Danno: " + damage);
+        return damage;
+    }
+
+    public boolean hasFainted() {
+        return this.hp <= 0;
+    }
+
+    public void displayHP() {
+        System.out.println(this.name + " Vita: " + this.hp);
+    }
+}
