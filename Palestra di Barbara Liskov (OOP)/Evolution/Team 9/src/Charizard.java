b28e2add9e671866b1d92e03637b2a8a371248cf
Filonzi Jacopo <jacopofilonzi@gmail.com>
2024-10-16 17:28:12 +0200

    Palestra Barbara Liskov OOP completa

---
+++ Palestra di Barbara Liskov (OOP)/Evolution/Team 9/src/Charizard.java
@@ -0,0 +1,8 @@
+
+public class Charizard extends Charmeleon
+{
+    public Charizard(String nome, String tipo, int livello, int puntiVita, String[] mosse)
+    {
+        super(nome, tipo, livello, puntiVita, mosse);
+    }
+}
