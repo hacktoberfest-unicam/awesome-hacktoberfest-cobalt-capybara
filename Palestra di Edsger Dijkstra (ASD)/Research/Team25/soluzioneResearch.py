186b8e73e1ac94a0cd1117109bae5aec5b34f106
Vales-cse <99ascione@gmail.com>
2024-10-16 15:58:21 +0200

    Aggiunta stampa della lista per testing della soluzione del Searching

--- Palestra di Edsger Dijkstra (ASD)/Research/Team25/soluzioneResearch.py
+++ Palestra di Edsger Dijkstra (ASD)/Research/Team25/soluzioneResearch.py
@@ -6,6 +6,12 @@ def linear_search(pokedex, name_or_id):
             return pokemon
     return None
 
+# Helper function to print the list of Pokémon
+def print_pokemons(pokemons):
+    for pokemon in pokemons:
+        print(pokemon)
+    print()
+
 # Main function to execute the testing of search
 def main():
     # Sample Pokedex (list of Pokémon with id and name)
@@ -16,6 +22,10 @@ def main():
         {"id": 4, "name": "Charmander"},
         {"id": 5, "name": "Pikachu"},
     ]
+
+    # Print the original list
+    print("List:")
+    print_pokemons(pokedex)
     
     ## Prompt user for input
     search_key = input("Enter the name or ID of the Pokémon to search: ")
