07d81475f981737e80842516326228f8acb16b1e
Filonzi Jacopo <jacopofilonzi@gmail.com>
2024-10-16 16:37:23 +0200

    Palestra Ritchie T.S.

---
+++ Palestra di Dennis Ritchie (IMP)/Trading System/Team 9/src/Pokemon.java
@@ -0,0 +1,15 @@
+public class Pokemon {
+    
+    String name; 
+    int hp;
+    int attack;
+    int defense;
+
+    public Pokemon(String name, int hp, int attack, int defense) {
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+    }
+    
+}
