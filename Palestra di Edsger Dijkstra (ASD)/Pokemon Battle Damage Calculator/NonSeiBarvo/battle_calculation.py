219c05fb9f704ebd64c047923711bcef0eb97476
uninicol <nicol.buratti@studenti.unicam.it>
2024-10-16 12:18:07 +0200

    battle calculator

--- Palestra di Edsger Dijkstra (ASD)/Pokemon Battle Damage Calculator/NonSeiBarvo/battle_calculation.py
+++ Palestra di Edsger Dijkstra (ASD)/Pokemon Battle Damage Calculator/NonSeiBarvo/battle_calculation.py
@@ -16,7 +16,7 @@ class Pokemon:
         self.type = type.lower()
         self.attack = attack
         self.defense = defense
-        self.moves = [{name.lower(): power} for name, power in moves]
+        self.moves = moves
 
     def calculate_damage(self, target: 'Pokemon'):
         damage = self.attack - target.defense
@@ -32,9 +32,22 @@ class Pokemon:
 # -------------------------
 
 def test_battle():
-    charizard = Pokemon("charizard", "fuoco", 100, 40, {})
-    carnivine = Pokemon("CARNIVINE", "erba", 50, 60, {})
+    charizard = Pokemon("charizard", "fuoco", 100, 40, [])
+    carnivine = Pokemon("CARNIVINE", "erba", 50, 60, [])
     damage = charizard.calculate_damage(carnivine)
     assert damage == (40*1.5)
+    print(f"{charizard.name} deals {damage} damage to {carnivine.name}!")
+
+    torterra = Pokemon("torterra", "erba", 60, 110, [])
+    damage = charizard.calculate_damage(torterra)
+    assert damage == 0
+    print(f"{charizard.name} deals {damage} damage to {torterra.name}!")
+
+
+    pikachu = Pokemon("pikachu", "elettro", 55, 40, [{"move_name": "Thunderbolt", "power": 40}])
+    charmander  = Pokemon("charmander ", "fuoco", 52, 43, [{"move_name": "Ember", "power": 40}])
+    damage = pikachu.calculate_damage(charmander)
+    assert damage == (55-43)
+    print(f"{pikachu.name} deals {damage} damage to {charmander.name}!")
 
 test_battle()
