2aaaffbe2ace55b519ce4bfe89c343721efe2006
HarlockOfficial <harlockofficial.github@gmail.com>
2024-10-16 15:29:49 +0200

    Update LevelCheck.py

--- Palestra di Dennis Ritchie (IMP)/Level Check/Team2/LevelCheck.py
+++ Palestra di Dennis Ritchie (IMP)/Level Check/Team2/LevelCheck.py
@@ -6,4 +6,19 @@ def sort_pokemon_levels(pokemon):
     odd_pokemon=[]
     for i in pokemon:
         if is_even(i):
-            ev
+            even_pokemon.append(i)
+        else:
+            odd_pokemon.append(i)
+    return even_pokemon, odd_pokemon
+
+risposta=""
+pokemon=[]
+while risposta!="exit":
+    risposta=input("Inserisci il livello del pokemon, 'exit' per uscire: ")
+    if risposta=="exit":
+        break
+    else:
+        pokemon.append(int(risposta))
+risultato=sort_pokemon_levels(pokemon)
+print("Pokemon con livello pari: ", risultato[0])
+print("Pokemon con livello dispari: ", risultato[1])
