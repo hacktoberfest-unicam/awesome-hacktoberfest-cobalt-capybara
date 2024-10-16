b301c16a913dec417c6b8f2a25ff8a832e175dac
damiano00 <pasquini.damiano00@gmail.com>
2024-10-16 11:19:50 +0200

    [done] imp level check

--- Palestra di Dennis Ritchie (IMP)/Level Check/LevelCheck.py
+++ Palestra di Dennis Ritchie (IMP)/Level Check/LevelCheck.py
@@ -24,3 +24,12 @@ def level_check(arr):
         else:
             odd.append(num)
     return even, odd
+
+def main():
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    even, odd = level_check(arr)
+    print("Even numbers:", even)
+    print("Odd numbers:", odd)
+
+if __name__ == "__main__":
+    main()
